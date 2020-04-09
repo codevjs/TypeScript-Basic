0681c91ad5d9e9a3782b3fafbbc19dfe5c27e537
codevjs <codevjs@gmail.com>
2020-04-09 18:14:17 +0800

    setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    entry: {
+        server: './src/index.ts'
+    },
+    output: {
+        filename: '[name].js',
+        path: __dirname + '/dist'
+    },
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use : "awesome-typescript-loader",
+            },
+            {
+                test: /\.js$/,
+                loader: 'source-map-loader',
+                enforce: 'pre'
+            }
+        ]
+    },
+    plugins: [],
+    mode: "development"
+};
