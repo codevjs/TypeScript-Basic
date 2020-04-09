11741c5cbd0ba20c7ddc49161e68f854f1598581
codevjs <codevjs@gmail.com>
2020-04-09 21:45:40 +0800

    abstract class

---
+++ src/example/abstract_class.ts
@@ -0,0 +1,22 @@
+abstract class Vehicle {
+    abstract  wheels : number;
+
+    start() : void {
+
+        console.log("Brum......");
+    }
+}
+
+class Car extends Vehicle {
+     wheels : number = 4;
+}
+
+class Motorcycle extends Vehicle {
+    wheels : number = 2
+}
+
+let car = new Car();
+car.start();
+
+let motorcycle = new Motorcycle();
+motorcycle.start();
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 // import './example/variable_&_dataType';
 // import './example/function_&_dataType';
-import './example/class_in_typescript'
+// import './example/class_in_typescript'
+import './example/abstract_class'
